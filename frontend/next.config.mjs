174fe8c5f3cf3770977362f145ac960524f74850
putra-pkwl03 <putrapongkowulu@gmail.com>
2026-07-26 20:44:51 +0700

    penyesuian seeder

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,45 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/dashboard",
-        destination: "/auth/login",
-        permanent: true,
-      },
-    ];
-  },
-
-  turbopack: {},
-
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.watchOptions = {
-        poll: 1000, // Cek perubahan berkas setiap 1 detik
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
-};
-
-export default nextConfig;
-
-
-
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   output: 'standalone',
-
-//   experimental: {
-//     cpus: 1,
-//   },
-
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-
 //   async redirects() {
 //     return [
 //       {
@@ -55,7 +15,7 @@ export default nextConfig;
 //   webpack: (config, { dev, isServer }) => {
 //     if (dev && !isServer) {
 //       config.watchOptions = {
-//         poll: 1000,
+//         poll: 1000, // Cek perubahan berkas setiap 1 detik
 //         aggregateTimeout: 300,
 //       };
 //     }
@@ -64,3 +24,43 @@ export default nextConfig;
 // };
 
 // export default nextConfig;
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+
+  experimental: {
+    cpus: 1,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/auth/login",
+        permanent: true,
+      },
+    ];
+  },
+
+  turbopack: {},
+
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
